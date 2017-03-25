743d40796f53cbacbd2e3d650fab1a5fecbe27ad
dcv4 <dcv4@alumnes.udl.cat>
2017-03-25 20:59:20 +0100

    Afegides entrades a la base de dades i probada la seva visualitzacio al navegador

--- Practica1/Aplicacio/views.py
+++ Practica1/Aplicacio/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, Http404
+
+from django.template import Context
+from django.template.loader import get_template
+
+from Aplicacio.models import Pelicula
 
 # Create your views here.
-
-from django.http import HttpResponse
  
 def foo(request):
     return HttpResponse("Hello World!")
+
+    
+def pelicules(request):
+    # Utilitzarem una planteilla prefixada per construir la pagina
+    template = get_template("dashboard1.html")
+    # template = get_template("dashboard2.html")
+    pelicula = Pelicula.objects.get(id=2241)
+    pelicules = Pelicula.objects.all()
+    
+    variables = Context({
+        "username": "Dani",
+        "author": "Luis Barcenas",
+        # "pelicula": pelicula
+        "pelicules": pelicules
+    })
+    page = template.render(variables)
+    return HttpResponse(page)
--- Practica1/Practica1/urls.py
+++ Practica1/Practica1/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from Aplicacio.views import foo
+from Aplicacio.views import foo, pelicules
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'Aplicacio/$', foo),
+    url(r'Aplicacio/Pelicules/$', pelicules),
 ]
