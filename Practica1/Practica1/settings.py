db54eeff4e05bb572be06ccb9ce99ad39ee6d02e
dcv4 <dcv4@alumnes.udl.cat>
2017-03-25 18:36:24 +0100

    Configuracio inicial

--- Practica1/Practica1/settings.py
+++ Practica1/Practica1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Practica1',
+    'Aplicacio',
 )
 
 MIDDLEWARE_CLASSES = (
