b026ff1e89a94628f06f884b5287520a5d3f2d88
dani <dani.uni1137@gmail.com>
2017-03-28 13:37:51 +0200

    Arreglat arxiu main_movies

--- Practica1/main_movies.py
+++ Practica1/main_movies.py
@@ -7,10 +7,7 @@ import json
 import requests
 from time import sleep
 
-import sys
-
 from Aplicacio.models import Pelicula
-
 
 api_key = None
 time = 0
@@ -20,7 +17,8 @@ class MoviesClient(object):
     """docstring for MoviesClient."""
 
     url_base = "http://comicvine.gamespot.com/api/characters/?api_key="
-    sufix = "&format=json&field_list=id,name,release_date,runtime,studios&offset="
+    sufix = ("&format=json" +
+             "&field_list=id,name,release_date,runtime,studios&offset=")
 
     def __init__(self, api_key):
         """Inicialitzar la clau."""
@@ -32,8 +30,10 @@ class MoviesClient(object):
         u"""Baixar-se la web."""
         base_urls = {
             "movies": "http://comicvine.gamespot.com/api/movies/?api_key=",
-            "characters": "http://comicvine.gamespot.com/api/characters/?api_key=",
-            "publishers": "http://comicvine.gamespot.com/api/publishers/?api_key=",
+            "characters": "http://comicvine.gamespot.com/api/characters/" +
+                          "?api_key=",
+            "publishers": "http://comicvine.gamespot.com/api/publishers/" +
+                          "?api_key=",
             "character": "",
             "movie": ""
         }
@@ -41,17 +41,14 @@ class MoviesClient(object):
         while not correcte:
             user_agent = {'User-agent': 'Mozilla/5.0'}
             if situation != "character" and situation != "movie":
-                url = base_urls[situation] + self.api_key + self.sufix + str(self.offset)
+                url = base_urls[situation] + self.api_key + self.sufix + \
+                    + str(self.offset)
             else:
                 url = url + "?api_key=" + self.api_key + "&format=json"
-            # print url
-            # try:
+
             resultat = requests.get(url, headers=user_agent)
             codi = resultat.status_code
-            # except:
-                # codi = -1
-                # print "ERROR AL CARREGAR!!!"
-                # print sys.exc_info()
+
             if codi == 200:
                 correcte = True
             elif codi == -1:
@@ -84,51 +81,30 @@ class MoviesClient(object):
             results = jsondata["results"]
             for result in results:
                 try:
-                    conversion = str(result["runtime"])
-                    pelicula = Pelicula(id=result["id"], nom=result["name"].encode("utf-8"),productors='null', data=result["release_date"].encode("utf-8"), durada=result["runtime"])
-                    # pelicula = Pelicula(id=1, nom='prueba',productors='null' data='hola', durada=10)
+                    str(result["runtime"])
+                    pelicula = Pelicula(id=result["id"],
+                                        nom=result["name"].encode("utf-8"),
+                                        productors='null',
+                                        data=result["release_date"].encode("utf-8"),
+                                        durada=result["runtime"])
                     if result["release_date"] is None:
                         print "Release_date es None"
                         print result["id"]
                         result["release_date"] = "null"
                     pelicula.save()
                 except:
-                    # print "Pues no a podido ser :("
-                    # print "   " + str(result["runtime"])
 
                     if result["release_date"] is None:
                         print "Release_date es None"
                         print result["id"]
                         result["release_date"] = "null"
 
-                    pelicula = Pelicula(id=result["id"], nom=result["name"].encode("utf-8"),productors='null', data=result["release_date"].encode("utf-8"), durada=0)
+                    pelicula = Pelicula(id=result["id"],
+                                        nom=result["name"].encode("utf-8"),
+                                        productors='null',
+                                        data=result["release_date"].encode("utf-8"),
+                                        durada=0)
                     pelicula.save()
-                if result["studios"] is not None:
-                    # print "   " + result["name"].encode("utf-8")
-                    for studio in result["studios"]:
-                        # print "     " + str(studio["name"])
-                        sleep(time)
-                    # try:
-                        # conversion = int(result["runtime"])
-                        # pelicula = Pelicula(id=result["id"], nom=result["name"].encode("utf-8"),productors='null', data=result["release_date"].encode("utf-8"), durada=result["runtime"])
-                        # # pelicula = Pelicula(id=1, nom='prueba',productors='null' data='hola', durada=10)
-
-                        # pelicula.save()
-                    # except:
-                        # print "Pues no a podido ser :("
-                    """
-                    new_url = result["api_detail_url"]
-                    data2 = self.requestData("movie", new_url)
-
-                    if data2 is not None:
-                        jsondata2 = json.loads(data2)
-                        characters = jsondata2["results"]["characters"]
-                        if characters is not None:
-                            for character in characters:
-                                print "       " + character["name"].encode("utf-8") + " - " + str(character["id"])
-
-                            print self.offset
-                    """
             if self.offset >= limit:
                 final = True
             else:
@@ -138,8 +114,7 @@ class MoviesClient(object):
         return resultat
 
 
-
-if __name__ == "__main__":
+def menu_inicial(api_key):
     if not api_key:
         try:
             api_key = sys.argv[1]
@@ -150,28 +125,18 @@ if __name__ == "__main__":
     mc = MoviesClient(api_key)
     print api_key
 
-    numero = int(input("Donam 1 per movies, 2 per characters o 3 per publishers: "))
+    numero = int(input("Donam 1 per movies, 2 per characters o 3 " +
+                       "per publishers: "))
     if numero == 1:
-        result = mc.movies()
+        mc.movies()
     elif numero == 2:
-        result = mc.characters()
+        mc.characters()
     elif numero == 3:
-        result = mc.publishers()
+        mc.publishers()
+
+
+if __name__ == "__main__":
+    menu_inicial(api_key)
 
 else:
-    print "main_movies.py is being imported into another module."
-
-    if not api_key:
-        key_file = open('api_key', 'r')
-        api_key = key_file.read().replace('\n', '')
-
-    mc = MoviesClient(api_key)
-    print api_key
-
-    numero = int(input("Donam 1 per movies, 2 per characters o 3 per publishers: "))
-    if numero == 1:
-        result = mc.movies()
-    elif numero == 2:
-        result = mc.characters()
-    elif numero == 3:
-        result = mc.publishers()
+    menu_inicial(api_key)
