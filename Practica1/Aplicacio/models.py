a635f704de3e10b74d6918d36275f75aaababa97
dani <dani.uni1137@gmail.com>
2017-04-18 12:12:04 +0200

    primera versio base del model de dades

--- Practica1/Aplicacio/models.py
+++ Practica1/Aplicacio/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    id = models.PositiveIntegerField(null=False, blank=False, primary_key=True)
+    name = models.TextField(max_length=50)
+    producers = models.TextField(max_length=100)
+    date = models.TextField(max_length=50)
+    duration = models.TextField(null=True, blank=True)
+    detail_url = models.TextField(max_length=100)
+
+    def __unicode__(self):
+        return self.nom
+
+class Character(models.Model):
+    id = models.PositiveIntegerField(null=False, blank=False, primary_key=True)
+    name = models.TextField(max_length=50)
+    gender = models.TextField(max_length=50)
+    description = models.TextField(max_length=100)
+    # id_pelicula = models.PositiveIntegerField(null=False, blank=False)
+
+    def __unicode__(self):
+        return self.nom
+
+class Location(models.Model):
+    id = models.PositiveIntegerField(null=False, blank=False, primary_key=True)
+    name = models.TextField(max_length=50)
+
+    def __unicode__(self):
+        return self.nom
+
+class Team(models.Model):
+    id = models.PositiveIntegerField(null=False, blank=False, primary_key=True)
+    name = models.TextField(max_length=50)
+    num_members = models.PositiveIntegerField(null=True, blank=True)
+    description = models.TextField(max_length=100)
+
+    def __unicode__(self):
+        return self.nom
+
+class Power(models.Model):
+    id = models.PositiveIntegerField(null=False, blank=False, primary_key=True)
+    name = models.TextField(max_length=50)
+
+    def __unicode__(self):
+        return self.nom
+
+class Relacions(models.Model):
+    id_movie = models.PositiveIntegerField(null=False, blank=False)
+    id_character = models.PositiveIntegerField(null=False, blank=False)
